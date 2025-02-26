a3ba868aa00480c994ee2cc2c8320971075d14fe
PiotrLas <piotr.las@protonmail.com>
2025-02-26 22:01:43 +0100

    create simple view for api app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    return Response({"message": "Hello world"})
+
+
