ccec1af54f56cf25380400d0bb5a8b72eb10b58d
piotr <piotr.las@protonmail.com>
2025-02-28 11:53:27 +0100

    add description to hydroponic system serializer

--- backend/systems/serializers.py
+++ backend/systems/serializers.py
@@ -24,6 +24,7 @@ class HydroponicSystemSerializer(serializers.ModelSerializer):
             'url',
             'pk',
             'name',
+            'description',
             'created_at',
             'updated_at',
         ]
